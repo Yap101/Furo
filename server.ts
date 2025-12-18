e3512446d7c092288e5bb62da029f5cfc33d61ce
JinYan <yapj-wm24@student.tarc.edu.my>
2025-12-18 19:41:37 +0800

    small detail change

--- server.ts
+++ server.ts
@@ -20,6 +20,10 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/api/transactions', async (req: express.Request, res: express.Response) => {
     const { address } = req.query;
